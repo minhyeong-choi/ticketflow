95bf3833c216fad99419e6ba958c8d421c332ee2
minhyeong-choi <minhyeong5112@gmail.com>
2026-08-03 12:31:55 +0900

    test: VenueSeatTest를 Testcontainers 기반으로 전환

--- src/test/java/com/ticket/ticketflow/domain/performance/entity/VenueSeatTest.java
+++ src/test/java/com/ticket/ticketflow/domain/performance/entity/VenueSeatTest.java
@@ -5,12 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class VenueSeatTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
 
     @Autowired
     private TestEntityManager em;
